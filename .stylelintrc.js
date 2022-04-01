088eb35514edeb067ce262e9a5cb4923834c7e65
cs-mi <halfmoonvic@gmail.com>
2022-04-01 13:07:31 +0800

    chore: add .browserslistrc

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -14,9 +14,6 @@ module.exports = {
   rules: {
     'plugin/no-low-performance-animation-properties': true,
     'plugin/declaration-block-no-ignored-properties': true,
-    'plugin/no-unsupported-browser-features': [
-      true,
-      { browsers: ['> 1%', 'Last 2 versions'], severity: 'warning' },
-    ],
+    'plugin/no-unsupported-browser-features': [true, { severity: 'warning' }],
   },
 };
